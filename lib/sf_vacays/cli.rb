201ca4838743be0002722af084c6b696a85a32a6
github username <github email address>
2019-08-21 14:17:16 -0400

    display_info functionality

--- lib/sf_vacays/cli.rb
+++ lib/sf_vacays/cli.rb
@@ -10,23 +10,20 @@ class SfVacays::CLI
 
  
     def list_vacays
-        # here doc - http://blog.jayfields.com/2006/12/ruby-multiline-strings-here-doc-or.html, https://en.wikibooks.org/wiki/Ruby_Programming/Here_documents
         puts "\nTop Autism Spectrum Friendly Family Vacations:"   
-        #SfVacays::Scraper.scrape_website
-        #SfVacays::Scraper.scrape_vacay
-        #binding.pry
-        @vacays = SfVacays::Scraper.scrape_website
-        @vacays.each.with_index(1) do |title, i|
-            puts "#{i}. #{title.text.blue}"
+        SfVacays::Scraper.scrape_website
+        SfVacays::Vacay.all.each.with_index(1) do |vacay, i|
+            puts "#{i}. #{vacay.title}"
+            #binding.pry
         end   
     end
 
     def display_info
-        @vacays = SfVacays::Scraper.scrape_website
-        @vacays.each.with_index do |el| 
-            info = el.next_element.text.each_line do |p| 
-                puts p.green
-            end
+        SfVacays::Vacay.all.each do |vacay| 
+            gets.chomp
+            puts "#{vacay.title}"
+            puts "#{vacay.info}"
+            puts "*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*".light_blue
         end
     end
 
@@ -35,12 +32,12 @@ class SfVacays::CLI
         while input != "exit"
             puts "Enter the number of the Vacation you'd like to explore between 1 to 30. Type list to see full list, or press exit:"
             input = gets.strip.downcase
-                if input.to_i > 0 && input.to_i <31 
-                    puts "*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*".light_blue
+                if input.to_i > 0 && input.to_i <30
                     display_info
-                    puts "*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*".light_blue
                 elsif input == "list"
-                    list_vacays
+                    SfVacays::Vacay.all.each.with_index(1) do |vacay, i|
+                        puts "#{i}. #{vacay.title}"
+                    end 
                 else
                     puts "Not sure what you want...Please enter in a location between 1 and 30, type list or exit.".red
                 end        
--- lib/sf_vacays/scraper.rb
+++ lib/sf_vacays/scraper.rb
@@ -23,11 +23,5 @@ class SfVacays::Scraper
         end       
     end 
 
-    def self.scrape_website(info)
-        @page = Nokogiri::HTML(open("https://www.appliedbehavioranalysisprograms.com/30-best-autism-friendly-vacation-ideas/"))       
-        @page.info
-        binding.pry
-    end
-
 end
 
