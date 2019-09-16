2c16faf48199b8be066847445da37ee85495100b
github username <github email address>
2019-09-16 13:24:46 -0400

    fixed functionality of description method

--- lib/sf_vacays/cli.rb
+++ lib/sf_vacays/cli.rb
@@ -4,7 +4,6 @@ class SfVacays::CLI
     def call  
         list_vacays
         description
-        #display_info
         goodbye
     end
 
@@ -13,18 +12,8 @@ class SfVacays::CLI
         puts "\nTop Autism Spectrum Friendly Family Vacations:"   
         SfVacays::Scraper.scrape_website
         SfVacays::Vacay.all.each.with_index(1) do |vacay, i|
-            puts "#{i}. #{vacay.title}"
-            #binding.pry
+            puts "#{i}. #{vacay.title}".blue
         end   
-    end
-
-    def display_info
-        SfVacays::Vacay.all.each do |vacay| 
-            gets.chomp
-            puts "#{vacay.title}"
-            puts "#{vacay.info}"
-            puts "*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*".light_blue
-        end
     end
 
     def description
@@ -32,13 +21,14 @@ class SfVacays::CLI
         while input != "exit"
             puts "Enter the number of the Vacation you'd like to explore between 1 to 30. Type list to see full list, or press exit:"
             input = gets.strip.downcase
-                if input.to_i > 0 && input.to_i <30
-                    display_info
-                elsif input == "list"
-                    SfVacays::Vacay.all.each.with_index(1) do |vacay, i|
-                        puts "#{i}. #{vacay.title}"
-                    end 
-                else
+                if input == "list"        
+                    list_vacays
+                elsif input.to_i > 0 && input.to_i < 30
+                    vacay = SfVacays::Vacay.all[input.to_i - 1] 
+                    puts "#{vacay.title}".light_blue
+                    puts  "#{vacay.info}".green
+                    puts "*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*".light_blue             
+                else 
                     puts "Not sure what you want...Please enter in a location between 1 and 30, type list or exit.".red
                 end        
         end
